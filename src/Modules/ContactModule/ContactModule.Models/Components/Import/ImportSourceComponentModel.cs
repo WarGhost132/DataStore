499e5812d03ed5e30d593e868c8b532d4fbe2c08
WarGhost132 <wawghostick@gmail.com>
2024-08-29 16:33:59 +0300

    Add import models

---
+++ src/Modules/ContactModule/ContactModule.Models/Components/Import/ImportSourceComponentModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ContactModule.Models.Components.Import;
+
+public sealed class ImportSourceComponentModel
+{
+    public List<ImportSourceItem> Sources { get; set; } = [];
+    public class ImportSourceItem
+    {
+        public string Id { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public SourceType Type { get; set; } = SourceType.ManualLoading;
+        public List<ImportPropertyComponentModel> Properties { get; set; }
+    }
+}
--- src/Modules/ContactModule/ContactModule.Models/GlobalUsings.cs
+++ src/Modules/ContactModule/ContactModule.Models/GlobalUsings.cs
@@ -2,3 +2,4 @@
 global using DataStore.Common.Enums;
 global using SharedModule.Models;
 global using ContactModule.Models.Components.Shared;
+global using Microsoft.AspNetCore.Components.Forms;
