e790aa3005fec610c66d2b7a1d267ea756553455
WarGhost132 <wawghostick@gmail.com>
2024-08-30 13:36:16 +0300

    Configure HttpClientConfiguration for localhost

--- src/App/DataStore.App/BuilderExtensions/HttpClientConfigurationBuilderExtension.cs
+++ src/App/DataStore.App/BuilderExtensions/HttpClientConfigurationBuilderExtension.cs
@@ -1,6 +1,26 @@
-﻿namespace DataStore.App.BuilderExtensions
+﻿namespace DataStore.App.BuilderExtensions;
+
+public static class HttpClientConfigurationBuilderExtension
 {
-    public class HttpClientConfigurationBuilderExtension
+    private static void SetupHttpClientConfiguration(this WebApplicationBuilder builder)
     {
+        builder.Services.AddHttpContextAccessor();
+
+        string? baseAddressUrl = builder.Configuration["HttpClientSettings:BaseAddressUrl"];
+        string backendHttpClientName = builder.Configuration["HttpClientSettings:BackendHttpClientName"];
+
+        builder.Services.AddHttpClient(
+            backendHttpClientName!,
+            client =>
+            {
+                if (baseAddressUrl != null)
+                {
+                    client.BaseAddress = new Uri(baseAddressUrl);
+                }
+            });
+
+        builder.Services.AddTransient(sp => sp
+            .GetRequiredService<IHttpClientFactory>()
+            .CreateClient(backendHttpClientName!));
     }
 }
